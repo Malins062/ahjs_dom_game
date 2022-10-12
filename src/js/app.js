41fe38bb590347a8b6c23ebb33b52065262e4e0a
Malins062 <6328673@gmail.com>
2022-10-12 17:14:40 +0300

    some changes

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,8 @@
-// TODO: write code here
+// const unusedVariable = 'variable';
+
+// for demonstration purpose only
+export default function demo(value) {
+  return value;
+}
+
+console.log('app.js included');  // eslint-disable-line
