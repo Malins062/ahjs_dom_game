fee76ec6bfa558412eaee4a91bb7364017909f97
Malins062 <6328673@gmail.com>
2022-10-13 17:13:38 +0300

    add class FieldsWidget

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,41 @@
+import FieldsWidge from './fields-widget';
+
+/* eslint-disable no-alert */
+const COUNT_FIELDS = 16; // Количество игровых полей
+
 console.log('Game loaded...');
 
 const titleGame = document.querySelector('.title');
+let playing = false;
 
 titleGame.addEventListener('click', () => {
-  console.log('Game start..');
-  
+  if (!playing) {
+    console.log('Game start..');
+
+    playing = true;
+    const widget = new FieldsWidget(document.querySelectorAll('.el'));
+    window.widget = widget;
+
+    // Старт игры
+    const gameInterval = setInterval(() => {
+      // Удаление из поля гоблина
+      widget.deactivateField();
+
+      // Вывод гоблина в новом поле
+      widget.activateField(Math.floor(1 + Math.random() * COUNT_FIELDS));
+    }, 1000);
+
+    // Обработка кликов на каждое поле
+    widget.allElements.forEach((element) => {
+      element.addEventListener('click', () => {
+        if (element.classList.contains('active')) {
+          clearInterval(gameInterval);
+          element.classList.add('dead');
+          alert('ПОЗДРАВЛЯЮ!!! ВЫ ЕГО ЗАВАЛИЛИ! ;-)');
+        }
+      });
+    });
+  } else {
+    alert('Игра уже запущена!');
+  }
 });
