edf6bcc1b288839a753ae36854f4981ee68538fa
Malins062 <6328673@gmail.com>
2022-10-13 12:01:54 +0300

    Change README

--- src/js/app.js
+++ src/js/app.js
@@ -3,5 +3,6 @@ console.log('Game loaded...');
 const titleGame = document.querySelector('.title');
 
 titleGame.addEventListener('click', () => {
-  console.log('Game start..');  // eslint-disable-line  
+  console.log('Game start..');
+  
 });
